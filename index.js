9332bd25cc7a0de10148809c7acd6be86dc93b5d
learnerdilip <diliptalks@gmail.com>
2020-04-04 16:45:53 +0200

    added css for menu overlay

---
+++ index.js
@@ -0,0 +1,7 @@
+const openNav = () => {
+  document.getElementById("menuoverlay").style.width = "100%";
+};
+
+const closeNav = () => {
+  document.getElementById("menuoverlay").style.width = "0%";
+};
